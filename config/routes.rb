02db0852cc69e27b5953585dcd891c03ed6656bd
Ali Murat Yildiz <info@oryasis.net>
2015-09-03 15:44:24 +0300

    changed design

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :weddings
 
-  root "pages#main"
+  root "weddings#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
